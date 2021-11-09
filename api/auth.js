69e8ab4dd464c74fc5ae4d80c5082f9334d1245d
BenSmile <benjkafirongo@gmail.com>
2021-11-09 12:22:47 +0100

    setup serverless.yml file

---
+++ api/auth.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+AWS.config.setPromisesDependency(require("bluebird"));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.submit = (event, callback) => {
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello to VDC app",
+    }),
+  });
+};
