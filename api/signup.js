0260fec5c2e6c70eabd780fc1e1581f4f63b16d7
BenSmile <benjkafirongo@gmail.com>
2021-11-09 17:52:50 +0100

    split signin and signup in two files

--- api/signup.js
+++ api/signup.js
@@ -4,55 +4,7 @@ const AWS = require("aws-sdk");
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.signin = async (event, callback) => {
-  if (!event.body) {
-    const rep = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Username and password are required",
-      }),
-    };
-    return rep;
-  }
-
-  const requestBody = JSON.parse(event.body);
-  const username = requestBody.username;
-  const password = requestBody.password;
-
-  if (!event.body) {
-    const rep = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Username and password are required",
-      }),
-    };
-    return rep;
-  }
-
-  if (typeof username !== "string" || typeof password !== "string") {
-    console.error("Validation Failed");
-    const rep = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Username or password required",
-      }),
-    };
-    return rep;
-  }
-
-  const params = {
-    TableName: 'USERS',
-    Key: {
-      username: username,
-    },
-  };
-
-  const result = await dynamoDb.get(params);
-
-  console.log('item = ',result?.Item);
-};
-
-module.exports.signup = (event, callback) => {
+module.exports.handler = (event, callback) => {
   const requestBody = JSON.parse(event.body);
   const fullname = requestBody.fullname;
   const email = requestBody.email;
