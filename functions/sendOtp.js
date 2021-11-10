7efc22bb8aaacb7a2f0d2aff78cee5a40ee9ba4d
BenSmile <benjkafirongo@gmail.com>
2021-11-10 17:40:18 +0100

    change sendOpt returned content-type

--- functions/sendOtp.js
+++ functions/sendOtp.js
@@ -2,17 +2,16 @@ const {sendSMS}  = require("../lib/sns");
 
 module.exports.handler = async function sendOtp(event) {
   const body = JSON.parse(event.body);
+
   return sendSMS(body.phoneNumber)
     .then((session) => ({
       statusCode: 200,
       body: JSON.stringify(session),
     }))
     .catch((err) => {
-      console.log({ err });
-
       return {
         statusCode: err.statusCode || 500,
-        headers: { "Content-Type": "text/plain" },
+        headers: { "Content-Type": "application/json" },
         body: { stack: err.stack, message: err.message },
       };
     });
