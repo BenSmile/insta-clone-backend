d945c747bf55d7bb584e0b02d474a660eec91bea
BenSmile <benjkafirongo@gmail.com>
2021-11-11 18:28:53 +0100

    add opt table

--- functions/sendOtp.js
+++ functions/sendOtp.js
@@ -1,5 +1,4 @@
 const { sendMail } = require("../lib/mail");
-const { sendSMS } = require("../lib/sns");
 
 module.exports.handler = async function sendOtp(event) {
   if (!event.body) {
@@ -8,32 +7,19 @@ module.exports.handler = async function sendOtp(event) {
       message: "Empty body",
     };
   }
-  const body = event.body;
-  if (body.mail) {
-    try {
-      await sendMail(body.mail);
-      return {
-        statusCode: 200,
-        message: "Activation code sent!",
-      };
-    } catch (error) {
-      return {
-        statusCode: 400,
-        message: "Error while sending Activation code",
-      };
-    }
-  } else if (body.phone) {
-    try {
-      sendSMS(body.phone);
-      return {
-        statusCode: 200,
-        message: "Activation code sent!",
-      };
-    } catch (error) {
-      return {
-        statusCode: 400,
-        message: "Error while sending Activation code",
-      };
-    }
+
+  const body = JSON.parse(event.body);
+
+  try {
+    await sendMail(body.mail);
+    return {
+      statusCode: 200,
+      message: "Activation code sent!",
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      message: "Error while sending Activation code",
+    };
   }
 };
