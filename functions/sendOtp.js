146ceeb966dc0e31bc36d199a4036c24546577b7
BenSmile <benjkafirongo@gmail.com>
2021-11-11 16:35:21 +0100

    setup mail for OTP

--- functions/sendOtp.js
+++ functions/sendOtp.js
@@ -1,18 +1,32 @@
-const {sendSMS}  = require("../lib/sns");
+const { default: sendMail } = require("../lib/mail");
+const { sendSMS } = require("../lib/sns");
 
 module.exports.handler = async function sendOtp(event) {
   const body = JSON.parse(event.body);
 
-  return sendSMS(body.phoneNumber)
-    .then((session) => ({
+  try {
+    sendMail(body.mail);
+    return {
       statusCode: 200,
-      body: JSON.stringify(session),
-    }))
-    .catch((err) => {
-      return {
-        statusCode: err.statusCode || 500,
-        headers: { "Content-Type": "application/json" },
-        body: { stack: err.stack, message: err.message },
-      };
-    });
+      message: "Activation code sent!",
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      message: "Error while sending Activation code",
+    };
+  }
+
+  // return sendSMS(body.phoneNumber)
+  //   .then((session) => ({
+  //     statusCode: 200,
+  //     body: JSON.stringify(session),
+  //   }))
+  //   .catch((err) => {
+  //     return {
+  //       statusCode: err.statusCode || 500,
+  //       headers: { "Content-Type": "application/json" },
+  //       body: { stack: err.stack, message: err.message },
+  //     };
+  //   });
 };
