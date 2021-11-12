f820822c4119d7a254c162aa711d1818c2ea10d4
BenSmile <benjkafirongo@gmail.com>
2021-11-12 10:47:50 +0100

    add validation to login enpoint

--- functions/login.js
+++ functions/login.js
@@ -3,12 +3,19 @@ const { login } = require("../lib/utils");
 module.exports.handler = async function signInUser(event) {
   if(!event.body){
     return {
-      statusCode: 204,
+      statusCode: 400,
       message: "Empty body",
     };
   }
   
   const body = JSON.parse(event.body);
+
+  if (!body.password || !body.username) {
+    return {
+      statusCode: 400,
+      message: "Username and password required",
+    };
+  }
 
   return login(body)
     .then(session => ({
