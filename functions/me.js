85d704c8d7763b11ab7b04805c4d148a426163c3
BenSmile <benjkafirongo@gmail.com>
2021-11-12 12:57:22 +0100

    fix issue when getting profile data

--- functions/me.js
+++ functions/me.js
@@ -1,21 +1,14 @@
 const { getUserByEmail } = require("../lib/db");
 const { getUserFromToken } = require("../lib/utils");
 
-module.exports.handler = async function (event) {
-  try {
-    const userObj = await getUserFromToken(event.headers.Authorization);
+module.exports.handler = async function(event) {
+  const userObj = await getUserFromToken(event.headers.Authorization);
 
-    const dbUser = await getUserByEmail(userObj.email);
+  const dbUser = await getUserByEmail(userObj.email);
 
-    return {
-      statusCode: 200,
-      headers: {},
-      body: JSON.stringify(dbUser),
-    };
-  } catch (error) {
-    return {
-      statusCode: 403,
-      message: "Not authorized",
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(dbUser)
+  };
 };
