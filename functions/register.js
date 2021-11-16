6ca4ec5ed452cee22ac959617bb4089957cd6403
BenSmile <benjkafirongo@gmail.com>
2021-11-16 15:35:18 +0100

    add return created user for step one

--- functions/register.js
+++ functions/register.js
@@ -1,20 +1,34 @@
-const { createDbUser } = require("../lib/db");
+const { createDbUser, getUserByEmail } = require("../lib/db");
 
 module.exports.handler = async function registerUser(event) {
   const body = JSON.parse(event.body);
 
-  return createDbUser(body)
-    .then(user => ({
-      statusCode: 200,
-      body: JSON.stringify(user)
-    }))
-    .catch(err => {
-      console.log({ err });
+  const user = await getUserByEmail(body.email);
+  console.log("user-> ", user);
 
-      return {
-        statusCode: err.statusCode || 500,
-        headers: { "Content-Type": "text/plain" },
-        body: { stack: err.stack, message: err.message }
-      };
-    });
+  if (user.email) {
+    console.log("user exists-> ", user);
+
+    return {
+      statusCode: 200,
+      message: "Email already taken",
+    };
+  } else {
+   return createDbUser(body)
+      .then((user) => {
+        console.log("createUser-- ", user);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(user),
+        };
+      })
+      .catch((err) => {
+        return {
+          statusCode: err.statusCode || 500,
+          headers: { "Content-Type": "text/plain" },
+          body: { stack: err.stack, message: err.message },
+        };
+      });
+  }
 };
