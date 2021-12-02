cac6137f4b623eb414c4147b08177fc4796d3c25
BenSmile <benjkafirongo@gmail.com>
2021-12-02 11:45:42 +0100

    log event

--- functions/posts.js
+++ functions/posts.js
@@ -1,160 +1,161 @@
 const {
-  createPost2,
-  getAllPosts,
-  likePost,
-  getAllPostsConnectedUser,
-  updatePost,
-  deletePost,
+    createPost2,
+    getAllPosts,
+    likePost,
+    getAllPostsConnectedUser,
+    updatePost,
+    deletePost,
 } = require("../lib/db");
-const { getUserFromToken } = require("../lib/utils");
+const {getUserFromToken} = require("../lib/utils");
 
 module.exports.create = async function addPost(event) {
-  if (!event.body) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Empty body" }),
-    };
-  }
-
-  if (!event.headers.Authorization) {
-    return {
-      statusCode: 403,
-      body: JSON.stringify({ message: "Plz, provide an token" }),
-    };
-  }
-  try {
-    const userObj = await getUserFromToken(event.headers.Authorization);
-    console.log("user-> ", userObj.email);
-    const body = JSON.parse(event.body);
-    await createPost2(body, userObj.email);
-    return {
-      statusCode: 200,
-      headers: {},
-      body: JSON.stringify({ message: "Post create successfully" }),
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error }),
-    };
-  }
-};
-
-module.exports.update = async function (event) {
-  if (!event.body) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Empty body" }),
-    };
-  }
-
-  if (!event.headers.Authorization) {
-    return {
-      statusCode: 403,
-      body: JSON.stringify({ message: "Plz, provide an token" }),
-    };
-  }
-
-  try {
-    const userObj = await getUserFromToken(event.headers.Authorization);
-    console.log("user-> ", userObj.email);
-    const body = JSON.parse(event.body);
-    await updatePost(body);
-    return {
-      statusCode: 200,
-      headers: {},
-      body: JSON.stringify({ message: "Post updated successfully" }),
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error }),
-    };
-  }
-};
-
-module.exports.allPosts = async function (event) {
-  let posts = await getAllPosts();
-
-  try {
-    if (event.headers.Authorization) {
-      const userObj = await getUserFromToken(event.headers.Authorization);
-      posts = await getAllPostsConnectedUser(userObj.email);
-    }
-  } catch (error) {
-    console.log(error);
-    posts = await getAllPosts();
-  }
-
-  return {
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify(posts),
-  };
-};
-
-module.exports.like = async function (event) {
-  try {
     if (!event.body) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "Empty body" }),
-      };
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: "Empty body"}),
+        };
     }
 
     if (!event.headers.Authorization) {
-      return {
-        statusCode: 403,
-        body: JSON.stringify({ message: "Not authorized" }),
-      };
+        return {
+            statusCode: 403,
+            body: JSON.stringify({message: "Plz, provide an token"}),
+        };
+    }
+    try {
+        const userObj = await getUserFromToken(event.headers.Authorization);
+        console.log("user-> ", userObj.email);
+        const body = JSON.parse(event.body);
+        await createPost2(body, userObj.email);
+        return {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({message: "Post create successfully"}),
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error}),
+        };
+    }
+};
+
+module.exports.update = async function (event) {
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: "Empty body"}),
+        };
     }
 
-    const body = JSON.parse(event.body);
-
-    if (!body.post) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: "post is required",
-        }),
-      };
+    if (!event.headers.Authorization) {
+        return {
+            statusCode: 403,
+            body: JSON.stringify({message: "Plz, provide an token"}),
+        };
     }
 
-    const userObj = await getUserFromToken(event.headers.Authorization);
+    try {
+        const userObj = await getUserFromToken(event.headers.Authorization);
+        console.log("user-> ", userObj.email);
+        const body = JSON.parse(event.body);
+        await updatePost(body);
+        return {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({message: "Post updated successfully"}),
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error}),
+        };
+    }
+};
 
-    await likePost(body.post, userObj.email);
+module.exports.allPosts = async function (event) {
+    let posts = await getAllPosts();
+
+    try {
+        if (event.headers.Authorization) {
+            const userObj = await getUserFromToken(event.headers.Authorization);
+            posts = await getAllPostsConnectedUser(userObj.email);
+        }
+    } catch (error) {
+        console.log(error);
+        posts = await getAllPosts();
+    }
+
     return {
-      statusCode: 200,
-      headers: {},
-      body: JSON.stringify({ message: "Post updated successfully" }),
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify(posts),
     };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error }),
-    };
-  }
+};
+
+module.exports.like = async function (event) {
+  console.log('event -> ', event);
+  try {
+        if (!event.body) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({message: "Empty body"}),
+            };
+        }
+
+        if (!event.headers.Authorization) {
+            return {
+                statusCode: 403,
+                body: JSON.stringify({message: "Not authorized"}),
+            };
+        }
+
+        const body = JSON.parse(event.body);
+
+        if (!body.post) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: "post is required",
+                }),
+            };
+        }
+
+        const userObj = await getUserFromToken(event.headers.Authorization);
+
+        await likePost(body.post, userObj.email);
+        return {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({message: "Post updated successfully"}),
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error}),
+        };
+    }
 };
 
 module.exports.delete = async function (event) {
-  try {
+    try {
+        console.log('event -> ', event);
+        const postId = event.pathParameters.postId;
 
-    const postId = event.pathParameters.postId;
+        if (event.headers.Authorization) {
+            const userObj = await getUserFromToken(event.headers.Authorization);
+            posts = await deletePost(postId);
 
-    if (event.headers.Authorization) {
-      const userObj = await getUserFromToken(event.headers.Authorization);
-      posts = await deletePost(postId);
-
-      return {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify({ message: "Post deleted successfully" }),
-      };
+            return {
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify({message: "Post deleted successfully"}),
+            };
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
